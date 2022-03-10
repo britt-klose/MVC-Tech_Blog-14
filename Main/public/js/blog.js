3db3f87548b886f72c9a55e5790e1438a579b98f
brittany klose <brittany.klose@yahoo.com>
2022-03-10 18:13:15 -0500

    funtions for updating blog and comments

---
+++ Main/public/js/blog.js
@@ -0,0 +1,115 @@
+// Updating blog post
+
+const updateBtnHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#blog-title').value.trim();
+  const description = document.querySelector('#blog-desc').value.trim();
+
+
+
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title, 
+        description, 
+      }),
+      headers: {
+      'Content-Type': 'application/json',
+    },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/blogs/${id}`);
+    } else {
+      alert('Failed to update post');
+    }
+  };
+
+
+//Creating new comment
+
+const newComment = async(event) => {
+  event.preventDefault();
+
+const description = document.querySelector('#comment-desc').value.trim();
+
+if (description) {
+  const response = await fetch(`/api/comments`, {
+    method: 'POST',
+    body: JSON.stringify({description }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to create comment');
+  }
+}
+};
+
+//Deleting a comment
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/comments/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete comment');
+      }
+    }
+  };
+
+
+//Updating your comment
+
+const updateHandler = async (event) => {
+    event.preventDefault();
+
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+
+      const description = document.querySelector('#comment-desc').value.trim();
+      
+      const response = await fetch(`/api/comments/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({description }),
+        headers: {
+        'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update comment');
+      }
+    }
+  };
+
+
+document
+  .querySelector('.edit-blog-form')
+  .addEventListener('submit', updateBtnHandler);
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newComment);
+
+document
+  .querySelector('.comment-list')
+  .addEventListener('click', delButtonHandler);
+
+// document
+//   .querySelector('.comment-list')
+//   .addEventListener('submit', updateHandler);
+
+
